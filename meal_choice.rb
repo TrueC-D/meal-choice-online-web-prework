720358f39b29a11aaf6243badc9f4c30d949a8db
Alethea Cary <aletheacary@gmail.com>
2019-05-05 22:55:41 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,5 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-def diet (meal_choice = "meat")
+def meal_choice(diet = "meat")
   puts meal_choice
 end
